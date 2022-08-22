3b3a073996d19bf244c2ac5e689a998b34106ec0
JarodEphec <j.boulangerflemal@students.ephec.be>
2022-08-22 14:35:21 +0200

    site php

--- WEB/dynamic/index.php
+++ WEB/dynamic/index.php
@@ -18,7 +18,7 @@ echo "Connection réussie" . "<br>";
 if ($result->num_rows >= 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "- id de commande: ". $row["commande_id"]."<br>". " - Jouet: " . $row["toy_name"]."<br>". "- Client: " . $row["user_name>
+    echo "- id de commande: ". $row["commande_id"]."<br>". " - Jouet: " . $row["toy_name"]."<br>". "- Client: " . $row["user_name"]. "<br>";
   }
 } else {
   echo "0 results";
@@ -26,3 +26,4 @@ if ($result->num_rows >= 0) {
 
 $conn->close();
 ?>
+
